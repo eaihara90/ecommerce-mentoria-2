1c8cd91066fa757425a323ceabaa7aa587ee51a0
Eduardo Aihara <eaihara90@gmail.com>
2025-09-14 20:41:56 -0300

    feat(ecommerce): integrate header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiModule } from '@ecommerce-mentoria-2/ui';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'ecommerce-mentoria-2-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    imports: [RouterModule, UiModule],
+    selector: 'ecommerce-mentoria-2-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ecommerce-mentoria-2';
+    title = 'ecommerce-mentoria-2';
 }
